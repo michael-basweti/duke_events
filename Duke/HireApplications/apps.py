43ecc76286760687d8829b22c67097bb79a9e1c6
Basweti <baswetima@gmail.com>
2019-06-27 23:20:32 +0300

    Create project

---
+++ Duke/HireApplications/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HireapplicationsConfig(AppConfig):
+    name = 'HireApplications'
---
+++ Duke/HireApplications/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+class Hire(models.Model):
+    SERVICE = (
+        ('tents', 'tents'),
+        ('catering', 'catering'),
+        ('electronics', 'electronics')
+    )
+    applicant_name = models.CharField(max_length=100, blank=False, null=False)
+    phone = PhoneNumberField()
+    email = models.EmailField(max_length=100, blank=False, null=False)
+    service = models.CharField(choices=SERVICE, max_length=20)
+    application_description = models.TextField(max_length=500, default='Your application', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
